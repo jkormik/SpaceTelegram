c867f596f7091f61df9f951bdb6fab7baebf5823
Andrew Sytchev <berryknoedel@gmail.com>
2021-09-10 22:34:23 +0300

    Take out format verification from download_picture function

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,17 +5,15 @@ from urllib.parse import urlparse, unquote
 
 
 def download_picture(picture_url, picture_endpoint):
-    format_from_link = get_format_from_link(picture_url)
-    if format_from_link == ".jpg" or format_from_link == ".png":
-        unquoted = unquote(picture_url)
-        parsed = urlparse(unquoted)
-        splited_path = os.path.split(parsed.path)
-        filename = splited_path[-1]
-        response = requests.get(picture_url)
-        response.raise_for_status()
-        pathlib.Path(picture_endpoint).mkdir(exist_ok=True)
-        with open(picture_endpoint+"/"+filename, "wb") as file:
-            file.write(response.content)
+    unquoted = unquote(picture_url)
+    parsed = urlparse(unquoted)
+    splited_path = os.path.split(parsed.path)
+    filename = splited_path[-1]
+    response = requests.get(picture_url)
+    response.raise_for_status()
+    pathlib.Path(picture_endpoint).mkdir(exist_ok=True)
+    with open(picture_endpoint+"/"+filename, "wb") as file:
+        file.write(response.content)
 
 
 def fetch_spacex_launch(spacex_launch_number, picture_endpoint):
@@ -23,7 +21,9 @@ def fetch_spacex_launch(spacex_launch_number, picture_endpoint):
     response.raise_for_status()
     links_to_spacex_imgs = response.json()["links"]["flickr_images"]
     for link in links_to_spacex_imgs:
-        download_picture(link, picture_endpoint)
+        format_from_link = get_format_from_link(link)
+        if format_from_link == ".jpg" or format_from_link == ".png":
+            download_picture(link, picture_endpoint)
 
 
 def get_format_from_link(link):
