aa1db8282ec8c8f8575bc636a6c8db0397dfd3f0
Andrew Sytchev <berryknoedel@gmail.com>
2021-09-16 12:09:42 +0300

    Enhance code readability

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -10,7 +10,7 @@ def download_picture(picture_url, picture_endpoint):
     filename = splited_path[-1]
     response = requests.get(picture_url)
     response.raise_for_status()
-    with open(picture_endpoint+"/"+filename, "wb") as file:
+    with open(f"{picture_endpoint}/{filename}", "wb") as file:
         file.write(response.content)
 
 
