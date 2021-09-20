2bc965a8a8b5639abe75cfa91ce33c870803b60c
Andrew Sytchev <berryknoedel@gmail.com>
2021-09-20 18:25:28 +0300

    Correct **kwargs naming

--- aiding_functions.py
+++ aiding_functions.py
@@ -3,12 +3,12 @@ from urllib.parse import urlparse, unquote
 import requests
 
 
-def download_picture(picture_url, picture_path, **kwargs):
+def download_picture(picture_url, picture_path, **params):
     unquoted = unquote(picture_url)
     parsed = urlparse(unquoted)
     splited_path = os.path.split(parsed.path)
     filename = splited_path[-1]
-    response = requests.get(picture_url, params=kwargs)
+    response = requests.get(picture_url, params=params)
     response.raise_for_status()
     with open(picture_path+"/"+filename, "wb") as file:
         file.write(response.content)
